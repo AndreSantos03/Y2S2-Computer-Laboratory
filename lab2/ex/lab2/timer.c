67f8e86b184da0a5c0d3679f480a015da3ff9ffd
Tiago Alexandre Oliveira <up202009302@up.pt>
2024-02-23 12:03:40 +0000

    Added lab0. Working.

---
+++ lab2/ex/lab2/timer.c
@@ -0,0 +1,48 @@
+#include <lcom/lcf.h>
+#include <lcom/timer.h>
+
+#include <stdint.h>
+
+#include "i8254.h"
+
+int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
+  if(timer>=0 && timer<=2) return 1;
+  19<=freq<=TIMER_FREQ;
+  uint16_t c = TIMER_FREQ/freq;
+  return 1;
+}
+
+int (timer_subscribe_int)(uint8_t *bit_no) {
+    /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int (timer_unsubscribe_int)() {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+void (timer_int_handler)() {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+}
+
+int (timer_get_conf)(uint8_t timer, uint8_t *st) {
+  if st == NULL return 1;
+  if(timer < 0 || timer > 2) return 1;
+  sys_outb(TIMER_CTRL,TIMER_RB_CMD|BIT(5)|TIMER_RB_SEL(timer));
+  util_sys_inb(addr_,st);
+  
+}
+
+int (timer_display_conf)(uint8_t timer, uint8_t st,
+                        enum timer_status_field field) {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
