5b222936f214b1662cf8a1f1e2a43dcf55963205
Tiago Martins <up202206640@up.pt>
2024-03-11 14:51:21 +0000

    Finished lab2, not tested

--- lab2/ex/lab2/utils.c
+++ lab2/ex/lab2/utils.c
@@ -4,21 +4,21 @@
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
   if(lsb==NULL) return 1;
-  *lsb=val&0x00FF
+  *lsb = val & 0xFF;
   return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
   if(msb==NULL) return 1;
-  *msb=val>>8;
+  *msb = val >> 8;
   return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
   if(value==NULL) return 1;
-  uint32_t i = 0;
-  int r = sys_inb(port;&i);
-  *value = i&0xFF;
+  uint32_t i;
+  int r = sys_inb(port, &i);
+  *value = i & 0xFF;
   return r;
   
 }
