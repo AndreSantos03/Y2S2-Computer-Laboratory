789412ad87886edfc85d6e1c377099af65e808ce
AndreSantos03 <andrebernardosantos@gmail.com>
2024-04-23 15:41:42 +0100

    Finished lab5

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -193,11 +193,62 @@ int(video_test_xpm)(xpm_map_t xpm, uint16_t x, uint16_t y) {
 
 int(video_test_move)(xpm_map_t xpm, uint16_t xi, uint16_t yi, uint16_t xf, uint16_t yf,
                      int16_t speed, uint8_t fr_rate) {
-  /* To be completed */
-  printf("%s(%8p, %u, %u, %u, %u, %d, %u): under construction\n",
-         __func__, xpm, xi, yi, xf, yf, speed, fr_rate);
+    if(video_init(0x105) != 0 ) return 1;
 
-  return 1;
+    bool vertical;
+    if (xi == xf && yi < yf) vertical = true;
+    else if (yi == yf && xi < xf) vertical = false;
+    else return 1;
+    int ipc_status;
+    message msg;
+
+    uint8_t irq_set_timer,irq_set_keyboard;
+
+    if( keyboard_subscribe_interrupts(&irq_set_keyboard) ) return 1;
+    if (timer_subscribe_int(&irq_set_timer) != 0) return 1;
+
+    if (timer_set_frequency(0, fr_rate) != 0) return 1;   
+
+
+    while(scancode != BREAK_ESC && (xi < xf || yi < yf)){
+        if( driver_receive(ANY, &msg, &ipc_status) != 0){
+            continue;
+        }
+        if(is_ipc_notify(ipc_status)){
+            switch(_ENDPOINT_P(msg.m_source)){
+                case HARDWARE:
+                if(msg.m_notify.interrupts & irq_set_keyboard){
+                    kbc_ih();
+                    break;
+                }
+                if(msg.m_notify.interrupts & irq_set_timer){
+                    //deletes formet movement
+                    if (draw_rectangle(xi, yi, 100, 100, 0xFFFFFF) != 0) return 1;
+
+                    if (vertical) {
+                        yi += speed;
+                        if (yi > yf) yi = yf;
+                    } else {
+                        xi += speed;
+                        if (xi > xf) xi = xf;
+                    }
+
+                    //draw new square
+                    if(draw_xpm(xpm, xi, yi) != 0) return 1;
+
+                    break;
+                }
+            }
+        }
+    }
+    
+    if (keyboard_unsubscribe_interrupts() != 0) return 1;
+    if (timer_unsubscribe_int() != 0) return 1;
+
+
+    if (vg_exit() != 0) return 1;
+
+    return 0;
 }
 
 int(video_test_controller)() {
