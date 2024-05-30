0b9c187e8addabea0795c17ff265d144d0288def
Tiago Alexandre Oliveira <up202009302@up.pt>
2024-05-30 16:25:30 +0100

    RTC implemented

--- lab6/rtc.c
+++ lab6/rtc.c
@@ -1,38 +1,74 @@
-#ifndef _LCOM_RTC_H_
-#define _LCOM_RTC_H_
+#include "rtc.h"
 
-#include <minix/sysutil.h>
-#include <lcom/lcf.h>
-
-#define SECONDS 0
-#define MINUTES 2
-#define HOURS 4
-#define DAYS 7
-#define MONTHS 8
-#define YEARS 9
-
-#define BINARY BIT(2)
-#define UPDATING BIT(7)
-
-#define RTC_MASK            5
-#define IRQ_RTC             8
-#define REGISTER_INPUT      0x70
-#define REGISTER_OUTPUT     0x71
-
-typedef struct {
-    uint8_t year;
-    uint8_t month;
-    uint8_t day;
-    uint8_t hours;
-    uint8_t minutes;
-    uint8_t seconds;
-} rtc_info;
+int rtc_hook_id = RTC_MASK;
+rtc_info info;
+uint8_t binary;
 
 
-int rtc_subscribe_interrupts();
-int rtc_unsubscribe_interrupts();
-int rtc_output(uint8_t command, uint8_t *output);
-int rtc_is_updating();
+int rtc_output(uint8_t command, uint8_t *output){
+    if(sys_outb(REGISTER_INPUT,command) != 0) return 1;
+        if(util_sys_inb(REGISTER_OUTPUT, output) != 0) return 1;
 
+    return 0;
+}
 
-#endif
+int rtc_is_updating(){
+    uint8_t res;
+    if(rtc_output(REGISTER_UPDATING, &result)) return 1;
+        return result & UPDATING;
+}
+
+int rtc_is_binary(){
+    uint8_t res;
+    if(rtc_output(COUNTING_STATUS_CMD, &result)) return 1;
+        return result & BINARY;
+}
+
+int rtc_is_bcd(){
+    return !rtc_is_binary();
+}
+
+uint8_t to_binary(uint8_t bcd){
+    return ((bcd >> 4) * 10) + (bcd & 0xF);
+}
+
+int rtc_subscribe_interrupts() {
+    return sys_irqsetpolicy(IRQ_RTC, IRQ_REENABLE, &rtc_hook_id);
+}
+
+int rtc_unsubscribe_interrupts() {
+    return sys_irqrmpolicy(&rtc_hook_id);
+}
+
+int rtc_update_time(){
+
+    //See if the RTC is busy, if it is we can't update the registers
+    if(rtc_is_updating() != 0) return 1;
+    uint8_t output;
+
+    //Seconds
+    if(rtc_output(SECONDS, &output) != 0) return 1;
+    info.seconds = binary ? output : to_binary(output);
+
+    //Minutes
+    if(rtc_output(MINUTES, &output) != 0) return 1;
+    info.minutes = binary ? output : to_binary(output);
+
+    //Hours
+    if(rtc_output(HOURS, &output) != 0) return 1;
+    info.hours = binary ? output : to_binary(output);
+
+    //Day
+    if(rtc_output(DAY, &output) != 0) return 1;
+    info.day = binary ? output : to_binary(output);
+
+    //Month
+    if(rtc_output(MONTH, &output) != 0) return 1;
+    info.month = binary_mode ? output : to_binary(output);
+
+    //Year
+    if(rtc_output(YEAR, &output) != 0) return 1;
+    info.year = binary_mode ? output : to_binary(output);
+
+    return 0;
+}
--- lab6/rtc.h
+++ lab6/rtc.h
@@ -1,26 +1,43 @@
-#include "rtc.h"
+#ifndef _LCOM_RTC_H_
+#define _LCOM_RTC_H_
 
-int rtc_hook_id = RTC_MASK;
-rtc_info info;
-uint8_t binary;
+#include <minix/sysutil.h>
+#include <lcom/lcf.h>
 
-int rtc_subscribe_interrupts() {
-    return sys_irqsetpolicy(IRQ_RTC, IRQ_REENABLE, &rtc_hook_id);
-}
+#define SECONDS 0
+#define MINUTES 2
+#define HOURS 4
+#define DAY 7
+#define MONTH 8
+#define YEAR 9
+#define UPDATE_STATUS_CMD 10
+#define COUNTING_STATUS_CMD 11
 
-int rtc_unsubscribe_interrupts() {
-    return sys_irqrmpolicy(&rtc_hook_id);
-}
+#define BINARY BIT(2)
+#define UPDATING BIT(7)
 
-int rtc_output(uint8_t command, uint8_t *output){
-    if(sys_outb(REGISTER_INPUT,command) != 0) return 1;
-        if(util_sys_inb(REGISTER_OUTPUT, output) != 0) return 1;
+#define RTC_MASK            5
+#define IRQ_RTC             8
+#define REGISTER_INPUT      0x70
+#define REGISTER_OUTPUT     0x71
 
-    return 0;
-}
+typedef struct {
+    uint8_t year;
+    uint8_t month;
+    uint8_t day;
+    uint8_t hours;
+    uint8_t minutes;
+    uint8_t seconds;
+} rtc_info;
 
-int rtc_is_updating(){
-    uint8_t res;
-    if(rtc_output(REGISTER_UPDATING, &result)) return 1;
-        return result & UPDATING;
-}
+int rtc_output(uint8_t command, uint8_t *output);
+int rtc_is_updating();
+int rtc_is_binary();
+int rtc_is_bcd();
+uint8_t to_binary(uint8_t bcd);
+int rtc_subscribe_interrupts();
+int rtc_unsubscribe_interrupts();
+int rtc_update_time();
+void rtc_setup();
+
+#endif
